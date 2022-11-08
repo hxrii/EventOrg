df2b913f83c3b391ba706150b2043b932527014d
Harikrishnan <harikrishnankannoth@gmail.com>
2022-11-09 03:13:13 +0530

    Smart Contract Finished

--- contracts/organiser.sol
+++ contracts/organiser.sol
@@ -18,13 +18,40 @@ contract EventContract{
 
     function CreateEvent(string memory name,uint date,uint price,uint TotalTickets) external {
     
-     require(date>block.timestamp,"Event has already PAST!!!");
-     require(TotalTickets>0,"Events need atleast 1 ticket");
+     require(date>block.timestamp,"WAIT!!! Event has already PAST!!!");
+     require(TotalTickets>0,"Hey, Events need atleast 1 ticket");
      events[nextId]=Event(msg.sender,name,date,price,TotalTickets,TotalTickets);
      nextId++;
         
 
     }
 
+    function buyTicket(uint id, uint Qty) external payable {
+        require(events[id].date!=0,"Sorry!!! The Event doesn't Exist.");
+        require(events[id].date>block.timestamp,"WAIT!!! Event has already PAST!!!"); 
+
+        Event storage _event = events[id];
+        require(msg.value==(_event.price*Qty),"Sorry!!! Not Enough balance");
+        require(_event.ticketLeft>=Qty,"Sorry!!! Tickets are SOLD OUT");
+        _event.ticketLeft -=Qty;
+        tickets[msg.sender][id]+=Qty;
+
+
+
+    }
+
+
+
+    function transfer(uint eventID, uint quantity, address to)external  
+    {
+        require(events[eventID].date!=0,"SORRY!!! The Event does not exist");
+        require(events[eventID].date>block.timestamp,"SORRY!!! The Event has ended");
+        require(tickets[msg.sender][eventID]>=quantity,"SORRY!!! You do not have Enough Tickets");
+        tickets[msg.sender][eventID] -= quantity;
+        tickets[to][eventID] += quantity;
+
+
+
+    }
 
 }
