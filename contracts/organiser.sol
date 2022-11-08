9a135263ac0ef627cc53cf21e4f009505b3d49d4
Harikrishnan <harikrishnankannoth@gmail.com>
2022-11-09 04:43:11 +0530

    Proper commenting and Descriptions provided

--- contracts/organiser.sol
+++ contracts/organiser.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.5.0 <0.9.0;
 
-contract EventContract{
-    struct Event{
+contract EventContract{    //Declaring Contract for event organisation
+    struct Event{          //Structure variables of Contract
         address organiser;
         string name;
         uint date;
@@ -12,12 +12,23 @@ contract EventContract{
 
     }
 
-    mapping(uint=>Event) public events;
-    mapping(address=>mapping(uint=>uint))public tickets;
-    uint public nextId;
+    mapping(uint=>Event) public events;  //Mapping index of event with each events
+
+    mapping(address=>mapping(uint=>uint))public tickets; //Mapping Addresses and Indexes for each events
+                                                         // to get each person's no of ticket for the event
+                                                         // Similar to 2D array, tickets[address][EventID] 
+                                                         // is mapped to the number of tickets of eventID's event
+                                                         // that the address holds
+
+
+    uint public nextId;  //variable for storing index values of each added events.
 
     function CreateEvent(string memory name,uint date,uint price,uint TotalTickets) external {
     
+        //Function to create Event 
+        //Event should have a future date and No of tickets should be > 0
+
+
      require(date>block.timestamp,"WAIT!!! Event has already PAST!!!");
      require(TotalTickets>0,"Hey, Events need atleast 1 ticket");
      events[nextId]=Event(msg.sender,name,date,price,TotalTickets,TotalTickets);
@@ -27,6 +38,12 @@ contract EventContract{
     }
 
     function buyTicket(uint id, uint Qty) external payable {
+
+        //Function to Buy Ticket 
+        //Event should have a valid future date and msg.sender should have enough balance
+        //Remaining tickets should be greater than or equal to Qty of purchase
+
+
         require(events[id].date!=0,"Sorry!!! The Event doesn't Exist.");
         require(events[id].date>block.timestamp,"WAIT!!! Event has already PAST!!!"); 
 
@@ -44,6 +61,9 @@ contract EventContract{
 
     function transfer(uint eventID, uint quantity, address to)external  
     {
+        // Should have valid eventID and event should have future date
+        // Also msg.sender's eventID tickets >= Quantity to be transfered
+        
         require(events[eventID].date!=0,"SORRY!!! The Event does not exist");
         require(events[eventID].date>block.timestamp,"SORRY!!! The Event has ended");
         require(tickets[msg.sender][eventID]>=quantity,"SORRY!!! You do not have Enough Tickets");
